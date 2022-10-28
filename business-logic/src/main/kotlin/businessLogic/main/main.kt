71d0e3a9a2661f2e7caed48ba5348899398bb827
John Scherer <82835446+jjscherer@users.noreply.github.com>
2022-10-27 21:45:14 -0400

    Delete business-logic directory

--- business-logic/src/main/kotlin/businessLogic/main/main.kt
+++
@@ -1,9 +0,0 @@
-import businessLogic.management.*
-
-fun main(){
-    val admin = Admin("JJ")
-    val movie = Movie("Tarzan")
-    admin.editMovie(movie,"Alaska")
-    movie.title = "lol"
-    println(movie.title)
-}
