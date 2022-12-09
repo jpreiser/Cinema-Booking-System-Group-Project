21ac1b04bef9505d173066ff12a852e64c8dc720
Brett Toutkoushian <brett.toutkoushian@uga.edu>
2022-12-08 19:53:35 -0500

    Added search.php file for real this time

---
+++ web/search.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Search</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+    <header id="header" role="banner">
+        <div id="header-left">
+	    <h1 href="landing.php" title="UGA Theaters">UGA Theaters</h1>
+	
+	</div>
+    </header>
+
+    <div id="navbar-1-wrap">
+        <nav id="navbar-1" class="topnav" role="navigation">
+	    <div id="navbar-1-left">
+	        <u1 id="nav-1" class="nav-1">
+		    <li id="menu-item-home">
+		        <a href="landing.php">Home</a>
+		    </li>
+		    <li id="menu-item-purchase">
+		        <a href="movies.php">Now Playing</a>
+                    </li>
+		    <li id="menu-item-login">
+		        <a href="index.php">Login</a>
+		    </li>
+		    <li id="menu-item-profile">
+		        <a href="profile.php">Profile</a>
+		    </li>
+		    <li id="menu-item-search">
+		        <input type="text" placeholder="Search for Movies">
+		    </li>
+		    <li id="menu-item-home" style="float: right;">
+		        <a href="logout.php">Logout</a>
+		    </li>
+		</u1>
+            </div>
+	</nav>
+    </div>
+</body>
